ed3bed7ba5e29f0494f4c3d489660bbd15d55e3d
Nithin0824 <nithinbr2408@gmail.com>
2026-09-07 12:10:07 +0000

    connection pooling

--- src/main/java/com/hotel/DBConnection.java
+++ src/main/java/com/hotel/DBConnection.java
@@ -1,26 +1,82 @@
 package com.hotel;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 public class DBConnection {
 
-    private static final String URL =
-            "jdbc:oracle:thin:@//172.30.0.173:1521/FREEPDB1";
-
-    private static final String USER = "HOTEL_USER";
-
-    private static final String PASSWORD = "Hotel@123";
-
-    public static Connection getConnection() throws SQLException {
+    public static Connection getConnection()
+            throws SQLException {
 
         try {
-            Class.forName("oracle.jdbc.OracleDriver");
-        } catch (ClassNotFoundException e) {
-            throw new SQLException("Oracle JDBC Driver not found!", e);
-        }
 
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+            Context initialContext =
+                    new InitialContext();
+
+            Context environmentContext =
+                    (Context) initialContext.lookup(
+                            "java:comp/env");
+
+            DataSource dataSource =
+                    (DataSource) environmentContext.lookup(
+                            "jdbc/HotelDB");
+
+            return dataSource.getConnection();
+
+        } catch (NamingException e) {
+
+            throw new SQLException(
+                    "Unable to obtain JNDI database connection.",
+                    e);
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//package com.hotel;
+//
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.SQLException;
+//
+//public class DBConnection {
+//
+//    private static final String URL =
+//            "jdbc:oracle:thin:@//172.30.0.173:1521/FREEPDB1";
+//
+//    private static final String USER = "HOTEL_USER";
+//
+//    private static final String PASSWORD = "Hotel@123";
+//
+//    public static Connection getConnection() throws SQLException {
+//
+//        try {
+//            Class.forName("oracle.jdbc.OracleDriver");
+//        } catch (ClassNotFoundException e) {
+//            throw new SQLException("Oracle JDBC Driver not found!", e);
+//        }
+//
+//        return DriverManager.getConnection(URL, USER, PASSWORD);
+//    }
+//}
